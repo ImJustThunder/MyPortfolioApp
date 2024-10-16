d2b15aba7d19150af764c80e4f36e367e53db036
Christopher Wilt <CTWilt@student.fullsail.edu>
2024-10-15 21:18:57 -0400

    ObservableObject

---
+++ Portfolio Fusion (Chris Wilt Portfolio)/Main/ObervableObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Portfolio_Fusion__Chris_Wilt_Portfolio_.Main
+{
+    internal class ObervableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
